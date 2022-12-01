3fbeeb29dc74a47f36704adb2306d992d704a4b5
ItsJustOla <oyeyemi.ola@gmail.com>
2022-12-01 22:12:36 +0000

    removed comment in topthree file

--- src/main/scala/day1/TopThree.scala
+++ src/main/scala/day1/TopThree.scala
@@ -7,20 +7,6 @@ object TopThree extends App {
   val fileName = "src/main/scala/day1/input.txt"
   val elfFoods = Source.fromFile(fileName).getLines().toList
 
-//  def calorieIter(list: List[String], elfCalSum: Int, largestCal: Int): Int = {
-//    list match {
-//      case Nil => largestCal
-//      case xs :: xy =>
-//        if (xs equals "")
-//          if (elfCalSum >= largestCal)
-//            calorieIter(xy, 0, elfCalSum)
-//          else
-//            calorieIter(xy, 0, largestCal)
-//        else
-//          calorieIter(xy, xs.toInt + elfCalSum, largestCal)
-//    }
-//  }
-
   def transformToListOfElfItemSums(list: List[String], elfCalSum: Int, finalList: List[Int]): List[Int] =
     list match {
       case Nil => finalList
